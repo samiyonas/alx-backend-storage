c9415e42c74a3312d2ea93ccc0d667946ff7f513
samiyonas <samuel.x.yonas@gmail.com>
2024-09-09 12:26:17 -0700

    need meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,4 +1,4 @@
 -- no table for a meeting
 CREATE OR REPLACE VIEW need_meeting AS
 SELECT name FROM students
-WHERE score > 80 AND (last_meeting IS NULL or last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
+WHERE score < 80 AND (last_meeting IS NULL or last_meeting < ADDDATE(CURDATE(), interval -1 MONTH));
