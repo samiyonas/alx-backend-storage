4fdd49652bfdd7ffa6bf112936dc085110ccb5e6
samiyonas <samuel.x.yonas@gmail.com>
2024-09-18 04:40:09 -0700

    implementing an expiring web cache and tracker

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/evn python3
+""" implementing an expiring web cache and tracker """
+import requests
+import redis
+
+
+def cache_page(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        r = redis.Redis()
+        key = "count:" + args[0]
+
+        result = f(*args, **kwargs)
+
+        if r.exists(key):
+            r.incr(key, ex=10)
+        else:
+            r.set(key, 1, ex=10)
+        return result
+    return wrapper
+
+
+@cache_page
+def get_page(url: str) -> str:
+    response = requests.get("http://slowwly.robertomurray.co.uk")
+
+    return response.text
